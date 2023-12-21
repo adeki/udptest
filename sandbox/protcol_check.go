c91f817bce08f54ee9db24aa2ba41d71fed74e2d
adeki <s.ikeda.1102@gmail.com>
2023-12-21 18:32:13 +0900

    fix check script

--- sandbox/protcol_check.go
+++ sandbox/protcol_check.go
@@ -11,10 +11,11 @@ func main() {
 		payload string
 	}{
 		{host: "chargen:19", payload: "\x00"},
-		{host: "dns:53", payload: "\x00\x00\x10\x00\x00\x00\x00\x00\x00\x00\x00\x00"},
+		//		{host: "dns:53", payload: "\x00\x00\x10\x00\x00\x00\x00\x00\x00\x00\x00\x00"},
 		{host: "ntp:123", payload: "\xE3\x00\x04\xFA\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xC5\x4F\x23\x4B\x71\xB1\x52\xF3"},
 		{host: "memcache:11211", payload: "\x00\x01\x00\x00\x00\x01\x00\x00version\r\n"},
-		{host: "nat-pmp:5351", payload: "\x00\x00"},
+		{host: "miniupnp:5351", payload: "\x00\x00"},
+		{host: "miniupnp:1900", payload: "M-SEARCH * HTTP/1.1\r\nHost: 239.255.255.250:1900\r\nMan: \"ssdp:discover\"\r\nMX: 5\r\nST: ssdp:all\r\n\r\n"},
 	}
 
 	for _, tc := range testcases {
